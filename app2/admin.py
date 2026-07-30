040662b09f62a62ee8b01579089277efb59cf8b2
DEVANANDRAI <devanandrai3260@gmail.com>
2026-07-30 17:23:23 +0530

    Updated admin pannel text

--- app2/admin.py
+++ app2/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from app2.models import *
 # Register your models here.
+admin.site.site_header = "Login System Administration"
+admin.site.site_title = "Login System Admin"
+admin.site.index_title = "Welcome to Login System Dashboard"
 admin.site.register(Student)
--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from user.models import *
 # Register your models here.
+
+
+admin.site.site_header = "Login System Administration"
+admin.site.site_title = "Login System Admin"
+admin.site.index_title = "Welcome to Login System Dashboard"
 admin.site.register(registration)
 admin.site.register(testtable)
